bde54811885bdc725929ea63de5c9eea9a9030fc
Murilo Sampaio <musilvasampaio15@gmail.com>
2026-04-13 20:58:12 -0300

    Style: Estou fazendo o sidebar

---
+++ sidebar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=], initial-scale=1.0">
+    <link rel="stylesheet" href="sidebarstyle.css">
+</head>
+<body>
+    <main>
+        <aside>
+            <img src="./frontend/assets/img/logos/Freehead shortLogo WO.svg" alt="" class="logo">
+            <img src="./frontend/assets/img/icons/search.svg" alt="">
+            <img src="./frontend/assets/img/icons/dashboard.svg" alt="">
+            <img src="./frontend/assets/img/icons/management.svg" alt="">
+            <img src="./frontend/assets/img/icons/payment.svg" alt="">
+            <img src="./frontend/assets/img/icons/infoStudent.svg" alt="">
+            <img src="./frontend/assets/img/icons/infoClass.svg" alt="">
+            <img src="./frontend/assets/img/icons/infoTeach.svg" alt=""> 
+            <img src="./frontend/assets/img/icons/infoAdm.svg" alt="">
+        
+        </aside>
+    </main>
+</body>
+</html>
