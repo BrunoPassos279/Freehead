4597a511ea3d4e32d984270e669d104c1b58bcdc
Murilo Sampaio <musilvasampaio15@gmail.com>
2026-04-24 14:17:50 -0300

    feature: Adicionei a sidebar como include, porém, não sei se está funcionando. Comecei a Dashboard

---
+++ dashboard.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+    <main>
+        <!-- SIDEBAR -->
+        <?php include './frontend/includes/sidebar.inc.php' ?>
+
+        <section>
+            <h1>Seja bem vindo <?php echo $usuario ?> ! </h1>
+             
+        </section>
+    </main>
+</body>
+</html>
---
+++ frontend/includes/sidebar.inc.php
@@ -0,0 +1,34 @@
+<aside class="sidebar">
+    <a href="" class="atalhos">
+        <img src="./frontend/assets/img/icons/search.svg" alt="">
+    </a>
+
+    <a href="" class="atalho">
+        <img src="./frontend/assets/img/icons/dashboard.svg" alt="">
+    </a>
+
+    <a href="" class="atalho">
+        <img src="./frontend/assets/img/icons/payment.svg" alt="">
+    </a>
+
+    <a href="" class="atalho">
+        <img src="./frontend/assets/img/icons/infoStudent.svg" alt="">          
+    </a>
+
+    <a href="" class="atalho">
+        <img src="./frontend/assets/img/icons/infoClass.svg" alt="">
+    </a>
+    
+    <a href="" class="atalho">
+        <img src="./frontend/assets/img/icons/infoTeach.svg" alt=""> 
+    </a>
+
+    <a href="" class="atalho">
+        <img src="./frontend/assets/img/icons/infoAdm.svg" alt="">
+    </a>
+
+    <a href="" class="atalho">
+       <img src="./frontend/assets/img/logos/Freehead shortLogo WO.svg" alt="" class="logo">
+    </a>
+
+    </aside>
